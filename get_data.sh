793237d9316d3299ec3e3cbe32e0cb47b8bbdffc
Andrea Telatin <andrea@telatin.com>
2019-02-20 16:13:08 +0000

    Description added

--- get_data.sh
+++ get_data.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+# A script to download a 16S amplicon dataset from Mothur
 
+echo " Downloading data from Mothur website "
 set -euxo pipefail
 
 wget "https://www.mothur.org/w/images/d/d6/MiSeqSOPData.zip"
